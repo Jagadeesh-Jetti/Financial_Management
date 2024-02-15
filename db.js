831d982a2cd7e08ca984fde157d2cf4785d23694
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-15 20:22:54 +0530

    refactor - updated db

--- db.js
+++ db.js
@@ -7,38 +7,18 @@ dotEnv.config({
 
 const mongoURI = process.env.MONGODB;
 
-console.log(mongoURI);
-
 const dataBase = () => {
   if (!mongoURI) {
-    console.error("Environment variable MONGODB not defined!");
+    console.error("Enviromente variable not defined!");
   } else {
     mongoose
       .connect(mongoURI)
-      .then(() => console.log("Connected to MongoDB"))
-      .catch((error) => console.error("Error connecting to MongoDB:", error));
-
-    // Connection Events
-    mongoose.connection.on("connected", () => {
-      console.log("Mongoose connected to db...");
-    });
-
-    mongoose.connection.on("error", (err) => {
-      console.log(err.message);
-    });
-
-    mongoose.connection.on("disconnected", () => {
-      console.log("Mongoose connection is disconnected...");
-    });
-
-    process.on("SIGINT", () => {
-      mongoose.connection.close(() => {
-        console.log(
-          "Mongoose connection is disconnected due to app termination..."
-        );
-        process.exit(0);
+      .then(() => {
+        console.log("Database connected.");
+      })
+      .catch((error) => {
+        console.error("Error while connecting database,", error);
       });
-    });
   }
 };
 
