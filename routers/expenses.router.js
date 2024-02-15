410b4b6c8bc34779695de0fb0f6290017c2a046c
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-15 20:39:32 +0530

    fix - fixed the expense post router

--- routers/expenses.router.js
+++ routers/expenses.router.js
@@ -1,27 +1,26 @@
-const express = require("express")
+const express = require("express");
 
-const ExpensesRouter = express.Router()
-const Expense = require("../models/expenses.model")
+const ExpensesRouter = express.Router();
+const Expense = require("../models/expenses.model");
 
 ExpensesRouter.get("/", async (req, res) => {
-    try{
-        const expenseData = await Expense.find()
-        res.json(expenseData)
-    }
-    catch(error){
-        res.status(500).json({ error : "Error while getting expense data:", error})
-    }
-})
+  try {
+    const expenseData = await Expense.find();
+    res.json(expenseData);
+  } catch (error) {
+    res.status(500).json({ error: "Error while getting expense data:", error });
+  }
+});
 
 ExpensesRouter.post("/", async (req, res) => {
-    const {description, amount, category} = req.body
-    try{
-        const newExpense = new Expense({ description, amount, category})
-        await newExpense.save() 
-    }
-    catch(error){
-        res.status(500).json({ error : "Error while adding expense:", error})
-    }
-})
+  const { description, amount, category } = req.body;
+  try {
+    const newExpense = new Expense({ description, amount, category });
+    await newExpense.save();
+    res.status(201).json(newExpense);
+  } catch (error) {
+    res.status(500).json({ error: "Error while adding expense:", error });
+  }
+});
 
-module.exports = ExpensesRouter
+module.exports = ExpensesRouter;
--- routers/income.router.js
+++ routers/income.router.js
@@ -14,7 +14,6 @@ IncomeRouter.get("/", async (req, res) => {
 
 IncomeRouter.post("/", async (req, res) => {
   const { description, amount, category } = req.body;
-
   try {
     const updateIncome = new Income({ description, amount, category });
     await updateIncome.save();
